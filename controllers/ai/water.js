5b8e560250c190931143692a662a2b2f59802d18
TheSolom <eslam.01212@gmail.com>
2023-10-15 18:17:19 +0300

    Temporarily removed authentication

--- controllers/ai/water.js
+++ controllers/ai/water.js
@@ -1,7 +1,7 @@
 import { make_prediction_portability } from "../../models/ai/script_portability.js";
 import { make_prediction_level } from "../../models/ai/script_level.js";
-import { make_prediction_treatment } from "../../models/ai/script_treatment.js";
 import { make_prediction_consumptionTank } from "../../models/ai/script_ConsumptionTank.js";
+// import { make_prediction_treatment } from "../../models/ai/script_treatment.js";
 
 export const postPortability = async (req, res, next) => {
   const output = await make_prediction_portability(req.body);
@@ -13,15 +13,15 @@ export const postLevel = async (req, res, next) => {
   res.status(200).json({ output });
 };
 
-export const postTreatment = (req, res, next) => {
-  make_prediction_treatment(req.body)
-    .then((output) => {
-      res.status(200).json({ output: output });
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-};
+// export const postTreatment = (req, res, next) => {
+//   make_prediction_treatment(req.body)
+//     .then((output) => {
+//       res.status(200).json({ output: output });
+//     })
+//     .catch((error) => {
+//       console.error("Error:", error);
+//     });
+// };
 
 export const postConsumptionTank = async (req, res, next) => {
   const output = await make_prediction_consumptionTank(req.body);
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,87 +1,87 @@
-import { validationResult } from "express-validator";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+// import { validationResult } from "express-validator";
+// import bcrypt from "bcryptjs";
+// import jwt from "jsonwebtoken";
+// import User from "../models/user.js";
 
-export const signup = async (req, res, next) => {
-  const errors = validationResult(req);
-  try {
-    if (!errors.isEmpty()) {
-      const error = new Error(
-        "Validation failed, Signing up data is incorrect"
-      );
-      error.statusCode = 422;
-      error.data = errors.mapped();
-      throw error;
-    }
+// export const signup = async (req, res, next) => {
+//   const errors = validationResult(req);
+//   try {
+//     if (!errors.isEmpty()) {
+//       const error = new Error(
+//         "Validation failed, Signing up data is incorrect"
+//       );
+//       error.statusCode = 422;
+//       error.data = errors.mapped();
+//       throw error;
+//     }
 
-    const { name, email, password } = req.body;
+//     const { name, email, password } = req.body;
 
-    const hashedPassword = await bcrypt.hash(password, 10);
-    if (hashedPassword) {
-      const newUser = new User({
-        name,
-        email,
-        password: hashedPassword,
-      });
-      await newUser.save();
-    }
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     if (hashedPassword) {
+//       const newUser = new User({
+//         name,
+//         email,
+//         password: hashedPassword,
+//       });
+//       await newUser.save();
+//     }
 
-    res.status(201).json({
-      message: "User created successfully",
-      userId: newUser._id,
-    });
-  } catch (err) {
-    if (!err.statusCode) err.statusCode = 500;
-    next(err);
-  }
-};
+//     res.status(201).json({
+//       message: "User created successfully",
+//       userId: newUser._id,
+//     });
+//   } catch (err) {
+//     if (!err.statusCode) err.statusCode = 500;
+//     next(err);
+//   }
+// };
 
-export const login = async (req, res, next) => {
-  const errors = validationResult(req);
-  try {
-    if (!errors.isEmpty()) {
-      const error = new Error(
-        "Validation failed, Signing up data is incorrect"
-      );
-      error.statusCode = 422;
-      error.data = errors.mapped();
-      throw error;
-    }
+// export const login = async (req, res, next) => {
+//   const errors = validationResult(req);
+//   try {
+//     if (!errors.isEmpty()) {
+//       const error = new Error(
+//         "Validation failed, Signing up data is incorrect"
+//       );
+//       error.statusCode = 422;
+//       error.data = errors.mapped();
+//       throw error;
+//     }
 
-    const { email, password } = req.body;
+//     const { email, password } = req.body;
 
-    const user = await User.findOne({ email });
+//     const user = await User.findOne({ email });
 
-    if (!user) {
-      const error = new Error("User not found");
-      error.statusCode = 401;
-      throw error;
-    }
+//     if (!user) {
+//       const error = new Error("User not found");
+//       error.statusCode = 401;
+//       throw error;
+//     }
 
-    const doMatch = await bcrypt.compare(password, user.password);
+//     const doMatch = await bcrypt.compare(password, user.password);
 
-    if (!doMatch) {
-      const error = new Error("Wrong password");
-      error.statusCode = 401;
-      throw error;
-    }
+//     if (!doMatch) {
+//       const error = new Error("Wrong password");
+//       error.statusCode = 401;
+//       throw error;
+//     }
 
-    const token = jwt.sign(
-      {
-        email: user.email,
-        userId: user._id.toString(),
-      },
-      "secret",
-      { expiresIn: "1h" }
-    );
+//     const token = jwt.sign(
+//       {
+//         email: user.email,
+//         userId: user._id.toString(),
+//       },
+//       "secret",
+//       { expiresIn: "1h" }
+//     );
 
-    res.status(200).json({
-      token: token,
-      userId: user._id,
-    });
-  } catch (err) {
-    if (!err.statusCode) err.statusCode = 500;
-    next(err);
-  }
-};
+//     res.status(200).json({
+//       token: token,
+//       userId: user._id,
+//     });
+//   } catch (err) {
+//     if (!err.statusCode) err.statusCode = 500;
+//     next(err);
+//   }
+// };
